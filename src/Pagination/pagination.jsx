0cba7383cc578fead054043f7f3108deccd0e098
Nastya <shamr44@mail.ru>
2023-08-27 19:07:59 +0300

    feat: add pagination and basic styles

---
+++ src/Pagination/pagination.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import styles from "./Pagination.module.sass"
+
+const Pagination = ({ coinsPerPage, totalCoins, paginate }) => {
+    const [isClicked, setIsClicked] = useState(false);
+    const pageNumbers = [];
+
+    for(let i = 1; i <= Math.ceil(totalCoins / coinsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+    
+    // const handleClick = () => {
+    //     setIsClicked(!isClicked);
+    // };
+
+    // const liClasses = {styles.pagination}
+
+    return (
+        <div>
+            <ul className={styles.pagination}>
+                { 
+                    pageNumbers.map(number => (
+                        <li className={styles.pagination__item} key={number}>
+                            <a href="#" className={styles.pagination__link} onClick={ () => paginate(number) }>
+                                {number}
+                            </a>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Pagination;
