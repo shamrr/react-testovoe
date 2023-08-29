91dc31c5ffeae75d9897d2f822cb1ea6bbbed9e4
Nastya <shamr44@mail.ru>
2023-08-30 02:31:21 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 import reactDom from "react-dom";
-
 import App from "./App/app";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Coin from "./Coin/coin";
 
 
-reactDom.render(<App/>, document.getElementById('root'));
+
+reactDom.render(
+    <Router>
+        <Routes>
+            <Route 
+                path="/" element={<App />} 
+            />
+            <Route 
+                path="/coin/:coinId" 
+                element={<Coin />} 
+            />
+        </Routes>
+    </Router>,
+    document.getElementById('root'),
+);
