9806c6e5b20c047251ef39e9eb9983661ea6e5fe
Nastya <shamr44@mail.ru>
2023-08-30 02:29:02 +0300

    feat: create portfel component, add param portfelItems - array with elements which added to portfel

---
+++ src/Portfel/portfel.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Portfel.module.sass"
+
+const Portfel = ({ portfelItems }) => {
+    return (
+        <div className={styles.portfel}>
+            <ul className={styles.portfel__list}>
+                {
+                    
+                    portfelItems.map(item => (
+                        <li className={styles.portfel__item} key={item.id}>
+                            {item}
+                            
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Portfel;
