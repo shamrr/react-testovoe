ac29a210ba5aebea91ddc70b96a9405c22d3e070
Nastya <shamr44@mail.ru>
2023-08-30 02:15:54 +0300

    commit

--- src/Button/button.jsx
+++ src/Button/button.jsx
@@ -4,7 +4,7 @@ import styles from "./Button.module.sass";
 const Button = (props) => {
     const {text = ''} = props;
 
-    return <button className={styles.btn}>{text}</button>;
+    return <button className={styles.btn} >{text}</button> 
 }
 
 export default Button;
